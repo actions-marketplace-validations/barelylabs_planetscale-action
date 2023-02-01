ec5fcebb4c24ce13da59f456a64ad0c389f1ff0e
Adam <adam@barely.io>
2023-02-01 15:20:54 -0500

    define and use input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { getInput, setOutput } from '@actions/core';
+
+const inputName = getInput('name');
+
+greet(inputName);
+
+function greet(name: string) {
+	return console.log(`Hello ${name}!`);
+}
